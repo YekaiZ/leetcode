419b3c3619e2ed112ccc7c4c27cde5f7c799a2d6
unknown <yekai7@hotmail.com>
2022-05-11 10:16:56 +0800

    wip rotate 2d array

---
+++ array/rotate2dArray.js
@@ -0,0 +1,17 @@
+const testData1 = [
+  [5, 1, 9, 11],
+  [2, 4, 8, 10],
+  [13, 3, 6, 7],
+  [15, 14, 12, 16],
+];
+
+const testData2 = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+const rotate2dArray = (matrix) => {};
+
+const result = rotate2dArray(testData2);
+console.log(result);
